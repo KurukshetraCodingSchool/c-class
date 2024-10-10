b2f85886ab9ef52b365434b71bc949cf83b5731f
Kurukshetra <kurukshetracodingschool@gmail.com>
2024-10-10 21:40:34 +0530

    Calculator Complete

--- Caluclator/advance.c
+++ Caluclator/advance.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <math.h>
+#include<stdlib.h>
+//  Function to set text color
+void setTextColor(const char* colorCode){
+    printf("%s",colorCode);
+}
+#define RED   "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE  "\033[0;314m"
+#define MANGENTA "\033[0;35m"
+#define CYAN "\033[0;36m"
+#define RESET "\033[0m" 
+
+
 int main()
 {
     char choice;
@@ -8,6 +22,13 @@ int main()
     double result;
     do
     {
+        #ifdef _WIN32  
+           system("cls");  // Windows
+        #else 
+          system("Clear")   // Linux / macOS 
+
+        #endif
+        setTextColor(CYAN);
         printf("Welcome to  My calculator !\n");
         printf("Press + for addition\n");
         printf("Press - for subtraction\n");
@@ -21,12 +42,13 @@ int main()
         printf("Press c for Trigonometric cos  \n");
         printf("Press t for Trigonometric tan \n");
         printf("Press 5 for exit\n");
-
         printf("Enetre a number for choice : ");
+     setTextColor(RESET);
         scanf(" %c", &choice);
         switch (choice)
         {
         case '+':
+          setTextColor(GREEN);
             printf("You Choose Addition\n");
             printf("Enter First Value numbers : ");
             scanf("%d", &a);
@@ -36,6 +58,7 @@ int main()
             break;
 
         case '-':
+        setTextColor(GREEN);
             printf("You Choose Subtraction\n");
             printf("Enter First Value numbers : ");
             scanf("%d", &a);
@@ -44,6 +67,7 @@ int main()
             printf("Subtraction : %d\n", a - b);
             break;
         case '*':
+        setTextColor(GREEN);
             printf("You choose Multiplication\n");
             printf("Enter First Value numbers : ");
             scanf("%d", &a);
@@ -53,6 +77,7 @@ int main()
             break;
 
         case '/':
+        setTextColor(GREEN);
             printf("You choose Division\n");
             printf("Enter First Value numbers : ");
             scanf("%d", &a);
@@ -64,11 +89,13 @@ int main()
             }
             else
             {
+                setTextColor(RED);
                 printf("Error!");
             }
             break;
 
         case '^':
+        setTextColor(GREEN);
             printf("Enter First Base : ");
             scanf("%d", &a);
             printf("Enter Second exponent : ");
@@ -78,6 +105,7 @@ int main()
             break;
 
         case 'S':
+        setTextColor(GREEN);
             printf("Enter First Base : ");
             scanf("%d", &a);
             if (a >= 0)
@@ -87,10 +115,12 @@ int main()
             }
             else
             {
+                setTextColor(RED);
                 printf("Error !");
             }
             break;
             case 'l':
+            setTextColor(GREEN);
             printf("Enter a  number : ");
             scanf("%d",&a);
             if (a>0){
@@ -98,11 +128,13 @@ int main()
                 printf("Natural logrithm (IN) of %.d is %.2lf ",a,result);
             }
             else{
+                setTextColor(RED);
                 printf("Error! Logarithm of a non-positive number is underfined. \n");
             }
             break;
 
             case 'e':
+            setTextColor(GREEN);
             printf("Enter a numebr : ");
             scanf("%d",&a);
             result =  exp(a);
@@ -110,6 +142,7 @@ int main()
             break;
 
             case 's':
+            setTextColor(GREEN);
             printf("Enter a number : ");
             scanf("%d",&a);
             result =sin(a);
@@ -117,12 +150,14 @@ int main()
             break;
 
             case 'c':
+            setTextColor(GREEN);
             printf("Enter a number : ");
             scanf("%d",&a);
             result =cos(a);
             printf("Cos of %d radians is %.2lf\n",a,result);
             break;
             case 't':
+            setTextColor(GREEN);
             printf("Enter a number : ");
             scanf("%d",&a);
             result =tan(a);
@@ -130,15 +165,19 @@ int main()
             break;
 
         default:
-            printf("Thank you!");
+        setTextColor(RED);
+            printf("Invalid Input Please try again later ! ");
             break;
         }
+        setTextColor(RESET);
         printf("\nDo you want to perform another calculation ? so please press (y/n) :  ");
           getchar();
-        scanf("%c",&choice1);
+        scanf(" %c",&choice1);
 
     } while (choice1 == 'y' || choice1 =='Y');
+    setTextColor(YELLOW);
     printf("Calculation Program has ended \n");
+    return 0;
 }
 
 
